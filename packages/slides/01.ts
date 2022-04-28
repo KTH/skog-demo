39c699ebfc0731592a26cdad13aa5b3666e277f6
Carlos Saito <carlos@saito.style>
2022-04-28 10:14:37 +0200

    Update slides

--- packages/slides/01.ts
+++ packages/slides/01.ts
@@ -1,20 +1,3 @@
 import express from "express";
 import log, { initializeLogger, skogMiddleware } from "skog";
 import { setTimeout } from "timers/promises";
-
-const app = express();
-// app.use(skogMiddleware);
-
-app.get("/", async (req, res) => {
-  log.info("Hello world!");
-  await something();
-  res.send("Hello world!");
-});
-
-async function something() {
-  await setTimeout(1000);
-  log.info("Something");
-}
-
-initializeLogger();
-app.listen(3000);
